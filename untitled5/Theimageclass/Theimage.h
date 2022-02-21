702c57f292e55c2ad608e1b5436dd4bd7b6cd065
qw225967 <396139919@qq.com>
2022-02-21 20:08:30 +0800

    new

--- untitled5/Theimageclass/Theimage.h
+++ untitled5/Theimageclass/Theimage.h
@@ -1,12 +1,12 @@
 //
 // Created by 方睿虔 on 2020/7/20.
 //
-#include "/usr/local/include/opencv4/opencv2/core/hal/interface.h"
-#include "/usr/local/include/opencv4/opencv2/core/cvdef.h"
-#include "/usr/local/include/opencv4/opencv2/core.hpp"       // 核心功能
-#include "/usr/local/include/opencv4/opencv2/imgproc.hpp"    // 图像处理模块
-#include "/usr/local/include/opencv4/opencv2/highgui.hpp"    // 读写图像和视频的函数，以及用户交互函数
-#include "/usr/local/include/opencv4/opencv2/highgui/highgui_c.h"
+#include "/opt/homebrew/Cellar/opencv/4.5.4_3/include/opencv4/opencv2/core/hal/interface.h"
+#include "/opt/homebrew/Cellar/opencv/4.5.4_3/include/opencv4/opencv2/core/cvdef.h"
+#include "/opt/homebrew/Cellar/opencv/4.5.4_3/include/opencv4/opencv2/core.hpp"       // 核心功能
+#include "/opt/homebrew/Cellar/opencv/4.5.4_3/include/opencv4/opencv2/imgproc.hpp"    // 图像处理模块
+#include "/opt/homebrew/Cellar/opencv/4.5.4_3/include/opencv4/opencv2/highgui.hpp"    // 读写图像和视频的函数，以及用户交互函数
+#include "/opt/homebrew/Cellar/opencv/4.5.4_3/include/opencv4/opencv2/highgui/highgui_c.h"
 
 
 using namespace cv;
--- untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/DependInfo.cmake
+++ untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/DependInfo.cmake
@@ -1,19 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/fangruiqian/CLionProjects/untitled5/Thecompare/Thecompare.cpp" "/Users/fangruiqian/CLionProjects/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/Thecompare/Thecompare.cpp.o"
-  "/Users/fangruiqian/CLionProjects/untitled5/Theimageclass/Theimage.cpp" "/Users/fangruiqian/CLionProjects/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/Theimageclass/Theimage.cpp.o"
-  "/Users/fangruiqian/CLionProjects/untitled5/main.cpp" "/Users/fangruiqian/CLionProjects/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/main.cpp.o"
+  "/Users/admin/go/src/github.com/thecompareimagecode/untitled5/Thecompare/Thecompare.cpp" "/Users/admin/go/src/github.com/thecompareimagecode/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/Thecompare/Thecompare.cpp.o"
+  "/Users/admin/go/src/github.com/thecompareimagecode/untitled5/Theimageclass/Theimage.cpp" "/Users/admin/go/src/github.com/thecompareimagecode/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/Theimageclass/Theimage.cpp.o"
+  "/Users/admin/go/src/github.com/thecompareimagecode/untitled5/main.cpp" "/Users/admin/go/src/github.com/thecompareimagecode/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../opencv2"
-  "/usr/local/include/opencv4"
+  "/opt/homebrew/Cellar/opencv/4.5.4_3/include/opencv4"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
